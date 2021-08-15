dc01b6239cbb05f6244f8ec4b0dcf286f805c95d
Liwei Yeh <liweiyehdev@gmail.com>
2021-08-15 21:31:45 +1000

    tab size changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/pages/Portfolio.module.css'
+import Head from "next/head";
+import styles from "../styles/pages/Portfolio.module.css";
+import utilStyles from "../styles/util/Util.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Liwei Yeh&apos;s Portfolio</title>
-        <meta name="description" content="Liwei Yeh's portfolio" />
-        <link rel="icon" href="/icons/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-       
-      </main>
-      <footer className={styles.footer}>
-
-      </footer>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Liwei Yeh&apos;s Portfolio</title>
+                <meta name="description" content="Liwei Yeh's portfolio" />
+                <link rel="icon" href="/icons/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+                <h1>This is style testing</h1>
+            </main>
+            <footer className={styles.footer}></footer>
+        </div>
+    );
 }
