7a0e78bf8043a3f9e5d13df6fc6581c4fa751b75
Liwei Yeh <liweiyehdev@gmail.com>
2021-09-29 22:52:15 +1000

    bio change

--- component/person/Person.tsx
+++ component/person/Person.tsx
@@ -3,25 +3,24 @@ import React, { useContext } from "react";
 // Scripts
 import scriptIndex from "../../public/scripts/scriptIndex";
 // CSS
-import styles from "../../styles/components/Skill.module.css";
+import styles from "../../styles/components/Person.module.css";
 import utilStyles from "../../styles/util/Util.module.css";
 // Context
 import { LanguageContext } from "../../context/LanguageContext";
 
 const Skill = () => {
     const { language } = useContext(LanguageContext);
-    const script = scriptIndex[language];
+    const script = scriptIndex[language].person;
 
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <div className={styles.characterContainer}></div>
-                <div className={styles.skillContainer}>
-                    <div className={styles.skillBox}>
-                        <h1 className={`${styles.title} ${utilStyles.h2}`}>
-                            {script.skillTitle}
-                        </h1>
+                <div className={styles.intro}>
+                    <div className={styles.left}>
+                        <h1 className={styles.title}>{script.title}</h1>
+                        <img />
                     </div>
+                    <p className={styles.content}>{script.content}</p>
                 </div>
             </main>
         </div>
